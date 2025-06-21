91fff1bb0d0db729da2795ee390b791ac7a92376
Reza <rezasatria289jakarta@gmail.com>
2025-06-21 15:58:55 +0700

    update-file

--- middleware.js
+++
@@ -1,8 +0,0 @@
-export function middleware(request) {
-  const url = new URL(request.url)
-  
-  // Jika request datang langsung (tidak melalui Cloudflare)
-  if (!request.headers.get('cf-worker')) {
-    return new Response('Access denied', { status: 403 })
-  }
-}
